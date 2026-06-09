44cb2e33a3823dbb76c85cc5f89d064bd6b7716c
nikolasimov <nsimov06@gmail.com>
2026-06-10 01:59:28 +0300

    small fixup

--- strata/http/static.py
+++ strata/http/static.py
@@ -7,27 +7,22 @@ class StaticFileHandler:
         self.base_dir = os.path.abspath(base_dir)
 
     def get_file(self, path):
-        # 1. root route mapping
+        
         if path == "/":
             path = "/index.html"
-
-        # 2. build full path
+            
         full_path = os.path.join(self.base_dir, path.lstrip("/"))
         full_path = os.path.abspath(full_path)
 
-        # 3. security check
         if not full_path.startswith(self.base_dir):
             return None, None
 
-        # 4. file existence check
         if not os.path.exists(full_path):
             return None, None
 
-        # 5. read file
         with open(full_path, "rb") as f:
             content = f.read()
 
-        # 6. mime type
         content_type, _ = mimetypes.guess_type(full_path)
         if content_type is None:
             content_type = "text/plain"
