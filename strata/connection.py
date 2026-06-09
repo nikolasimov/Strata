026aacc8fa25865a75137f015123349dc3db5f6d
nikolasimov <nsimov06@gmail.com>
2026-06-10 01:36:33 +0300

    added static website demo(index + style)

--- strata/connection.py
+++ strata/connection.py
@@ -1,3 +1,5 @@
+from strata.http.static import StaticFileHandler
+from strata.http.response import HTTPResponse
 from strata.http.parser import HTTPParser
 
 
@@ -8,13 +10,27 @@ class Connection:
         self.router = router
 
     def handle(self):
-            data = self.client_socket.recv(1024)
+        data = self.client_socket.recv(1024)
 
-            parser = HTTPParser()
-            request = parser.parse(data)
+        parser = HTTPParser()
+        request = parser.parse(data)
 
-            handler = self.router.resolve(request)
-            response = handler(request)
+        static = StaticFileHandler()
 
+        file_content, content_type = static.get_file(request.path)
+        
+        if file_content:
+            response = HTTPResponse(
+                200,
+                {"Content-Type": content_type},
+                file_content
+            )
             self.client_socket.sendall(response.to_bytes())
             self.client_socket.close()
+            return
+
+        handler = self.router.resolve(request)
+        response = handler(request)
+
+        self.client_socket.sendall(response.to_bytes())
+        self.client_socket.close()
